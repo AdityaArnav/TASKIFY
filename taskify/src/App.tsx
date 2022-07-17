00b01fd113b6deec28b2f145318255794f779844
AdityaArnav <99977930+AdityaArnav@users.noreply.github.com>
2022-07-17 18:32:31 +0530

    some changes

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputFeild from './components/InputFeild';
+import { Todo } from './components/model';
 
 const App:React.FC=()=> {
+  const [todo,setTodo]= useState<string>('');
+  const [todos,setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent)=>{
+    e.preventDefault();
+  };
+  console.log(todo)
   return (
     <div className="App">
      <span className='heading'>Taskify</span>
-     <InputFeild/>
+     <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   );
 }
--- taskify/src/components/InputFeild.tsx
+++ taskify/src/components/InputFeild.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './styles.css';
 
-const InputFeild = () => {
+interface Props{
+  todo:string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd:(e:React.FormEvent)=> void;
+}
+
+const InputFeild = ({todo,setTodo,handleAdd}:Props) => {
   return (
     <div>
-        <form className='input'>
-      <input type='input' placeholder='Enter a task' className='input__box'/>
+        <form className='input' onSubmit={handleAdd}>
+      <input type='input' placeholder='Enter a task' className='input__box' value={todo} onChange={(e)=>setTodo(e.target.value)}/>
       <button className='input_submit' type='submit'>Go</button>
         </form>
     </div>
